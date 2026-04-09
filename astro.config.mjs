69e5235417975dd3f16f93ea4a69a02b550293c2
Gerard <256761544+grd23cd@users.noreply.github.com>
2026-04-09 16:38:44 +0800

    Add base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://grd23cd.github.io/astro-test',
+  base: '/astro-test/',
 
   vite: {
     plugins: [tailwindcss()]
